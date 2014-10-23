fd8d9c923623e7d1612fca9584d6a258dae5c895
Kill Your TV <killyourtv@i2pmail.org>
2014-10-23 18:25:45 +0000

    Add default hosts.txt file
    The hosts.txt has been copied over from the Java I2P router.
    
    Signed-off-by: Kill Your TV <killyourtv@i2pmail.org>

---
+++ debian/i2pd.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+set -u
+set -e
+
+_HOME=$HOME/.i2pd
+HOSTS=/usr/share/doc/i2pd/examples/hosts.txt.gz
+
+[ -d $_HOME] || mkdir $_HOME
+
+if [ ! -r $_HOME/hosts.txt ]; then
+    zcat $HOSTS > $_HOME/hosts.txt
+fi
+
+exec /usr/lib/i2pd/i2p "$@"
